0508b9415baece42d597b52ec7f2bbd7d1001f88
Luiz Santos <lfp.santos543@gmail.com>
2023-10-19 21:43:12 -0300

    commit inicial

---
+++ src/Iphone.java
@@ -0,0 +1,46 @@
+public class Iphone implements AparelhoTelefonico, ReprodutorMusical, NavegadorInternet{
+    @Override
+    public void ligar(String numero) {
+        //...
+    }
+
+    @Override
+    public void atender(String numero) {
+        //...
+    }
+
+    @Override
+    public void iniciarCorreioVoz(String mensagem, String destinatario) {
+        //...
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        //...
+    }
+
+    @Override
+    public void adicionarNovaAba(String url) {
+        //...
+    }
+
+    @Override
+    public void atualizarPagina() {
+        //...
+    }
+
+    @Override
+    public void tocar() {
+        //...
+    }
+
+    @Override
+    public void pausar() {
+        //...
+    }
+
+    @Override
+    public void selecionarMusica() {
+        //...
+    }
+}
